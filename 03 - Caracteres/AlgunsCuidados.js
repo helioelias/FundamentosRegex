f01691f17457a1468df253c23e934c701c995cc7
Helio Elias <helioelias@gmail.com>
2019-01-03 22:48:40 -0200

    Fundamentos Regex - 3. Caracteres Aula de 1 a 5

---
+++ 03 - Caracteres/AlgunsCuidados.js
@@ -0,0 +1,10 @@
+
+const textoUmaLinha = '...' // aspas simples ou duplas
+
+const textoMultiLinha = `
+    linha 1
+    linha 2
+`
+
+// cuidado como tab
+console.log('	'.match(/\s/g))
---
+++ 03 - Caracteres/OlaRegex.js
@@ -0,0 +1,7 @@
+const texto = 'Casa bonita é a casa amarela da esquina com a Rua ACASALAR.'
+
+const regex = /casa/gi
+
+console.log(texto.match(regex))
+
+console.log(texto.match(/a b/))
